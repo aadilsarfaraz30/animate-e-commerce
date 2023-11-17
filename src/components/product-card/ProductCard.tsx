000d3a0d8993d50e6d0c22768af51ce332b512e0
Aadil Sarfaraz <aadil@Aadils-MacBook-Air.local>
2023-11-17 10:01:21 +0530

    fixed scaling

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './ProductCard.scss';
 
-const ProductCard = ({ url, title, user, description, id }:any) => {
+const ProductCard = ({ url, title }: any) => {
+  const cardRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e: any) => {
+      if (!cardRef.current) return;
+
+      const card = cardRef.current;
+      const cardRect = card.getBoundingClientRect();
+      const offsetX = (e.clientX - cardRect.left) / cardRect.width - 0.5;
+      const offsetY = (e.clientY - cardRect.top) / cardRect.height - 0.5;
+
+      card.style.transform = `translate(${offsetX * 20}px, ${offsetY * 20}px)`;
+    };
+
+    const handleMouseLeave = () => {
+      if (!cardRef.current) return;
+
+      const card = cardRef.current;
+      card.style.transform = 'translate(0, 0)';
+    };
+
+    if (cardRef.current) {
+      // Explicitly cast the event type to 'any' here
+      cardRef.current.addEventListener('mousemove', handleMouseMove as any);
+      cardRef.current.addEventListener('mouseleave', handleMouseLeave);
+    }
+
+    return () => {
+      if (cardRef.current) {
+        cardRef.current.removeEventListener('mousemove', handleMouseMove as any);
+        cardRef.current.removeEventListener('mouseleave', handleMouseLeave);
+      }
+    };
+  }, []);
+
   return (
-    <div className="product-card">
+    <div className="product-card" ref={cardRef}>
       <img src={url} alt={title} className="product-image" />
       <div className="product-details">
         <h2 className="product-title">{title}</h2>
